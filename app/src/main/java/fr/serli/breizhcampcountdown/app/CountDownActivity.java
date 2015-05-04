710ec9d850e146b76e6f1621a188d276ab23d670
Nat-Faeeria <Nat-Faeeria@users.noreply.github.com>
2015-05-04 14:04:19 +0200

    basic countdown working with coloration

---
+++ app/src/main/java/fr/serli/breizhcampcountdown/app/CountDownActivity.java
@@ -0,0 +1,140 @@
+package fr.serli.breizhcampcountdown.app;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.CountDownTimer;
+import android.widget.TextView;
+import fr.serli.breizhcampcountdown.app.util.SystemUiHider;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+
+public class CountDownActivity extends Activity {
+    /**
+     * Whether or not the system UI should be auto-hidden after
+     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
+     */
+    private static final boolean AUTO_HIDE = true;
+
+    /**
+     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
+     * user interaction before hiding the system UI.
+     */
+    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+
+    /**
+     * If set, will toggle the system UI visibility upon interaction. Otherwise,
+     * will show the system UI visibility upon interaction.
+     */
+    private static final boolean TOGGLE_ON_CLICK = true;
+
+    /**
+     * The flags to pass to {@link SystemUiHider#getInstance}.
+     */
+    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
+
+    /**
+     * The instance of the {@link SystemUiHider} for this activity.
+     */
+    private SystemUiHider mSystemUiHider;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_count_down);
+
+        final View contentView = findViewById(R.id.fullscreen_content);
+
+        // Set up an instance of SystemUiHider to control the system UI for
+        // this activity.
+        mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
+        mSystemUiHider.setup();
+
+
+        // Set up the user interaction to manually show or hide the system UI.
+        contentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TOGGLE_ON_CLICK) {
+                    mSystemUiHider.toggle();
+                } else {
+                    mSystemUiHider.show();
+                }
+            }
+        });
+
+
+        Intent receptedIntent = getIntent();
+        Long duration = receptedIntent.getLongExtra("duration",3);
+
+        Long durationInMillis = duration*60000;
+
+        new CountDownTimer(durationInMillis, 1000) {
+            public void onTick(long millisUntilFinished){
+                long minutes = millisUntilFinished/60000;
+                long secondes = (millisUntilFinished%60000)/1000;
+                ((TextView) contentView).setText(minutes+":"+secondes);
+
+                if (millisUntilFinished<=300000){
+                    findViewById(R.id.cd_frame_layout).setBackgroundColor(Color.rgb(240,250,60));
+                }
+            }
+
+            public void onFinish(){
+
+                ((TextView) contentView).setText("done");
+                findViewById(R.id.cd_frame_layout).setBackgroundColor(Color.RED);
+            }
+        }.start();
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // Trigger the initial hide() shortly after the activity has been
+        // created, to briefly hint to the user that UI controls
+        // are available.
+        delayedHide(100);
+    }
+
+
+    /**
+     * Touch listener to use for in-layout UI controls to delay hiding the
+     * system UI. This is to prevent the jarring behavior of controls going away
+     * while interacting with activity UI.
+     */
+    View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if (AUTO_HIDE) {
+                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+            }
+            return false;
+        }
+    };
+
+    Handler mHideHandler = new Handler();
+    Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            mSystemUiHider.hide();
+        }
+    };
+
+    /**
+     * Schedules a call to hide() in [delay] milliseconds, canceling any
+     * previously scheduled calls.
+     */
+    private void delayedHide(int delayMillis) {
+        mHideHandler.removeCallbacks(mHideRunnable);
+        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+}
